aed5e48d15f74a7f692445c934b385de20de710b
ojikoki57 <t236010@st.yamagata-u.ac.jp>
2024-10-11 19:10:56 +0900

    Add files via upload

---
+++ 課題２.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello. ");
+    
+    int sisutemusousei;
+    sisutemusousei = 23516010;
+    
+    printf("sisutemusousei is: %d", sisutemusousei);
+    
+    return 0;
+}
