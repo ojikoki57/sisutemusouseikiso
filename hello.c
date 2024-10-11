901a50dac561c0e330cce840180f385f17ed8361
ojikoki57 <t236010@st.yamagata-u.ac.jp>
2024-10-11 18:47:28 +0900

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,7 @@
+:;easjypejgheajhaehigan:airozjgha:j
+akrejg;eih;aoirwehntlajbnt;awetg
+kasrhgl;aerog;aeoih
+itkhjosri;ughsoeijtojhysreiophjy
+;sotihjgo;sihtnhylkesh;yi
+;iorjty;oesihytse;oihy
+sotuihjso;tyhjslknhoietjyhse;oijykesj
